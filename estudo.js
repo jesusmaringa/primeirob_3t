530c709af41cbade7a436f10cc838310fa5769d8
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-05 12:46:40 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -13,3 +13,22 @@ function escreva(){
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
 
+let lista = ["Jesus","Spessato","Duarte","Jorge","Neuza","Gilmar","Jossyara"];
+
+function mostraLista(){
+    document.write("Tamanho da lista: " + lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write("Professor: " + lista[i] + "<br>");
+    }
+}
+
+function multiplica(){
+    for(let i = 6; i <= 8; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x "+j+" = " + (i*j) + "<br>");
+        }
+        document.write("<br>");
+    }
+}
+
